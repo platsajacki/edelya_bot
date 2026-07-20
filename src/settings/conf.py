1bf121b46c4a62b9f5e604352c5af51491d13427
Vyacheslav Menyukhov <plasajacki@gmail.com>
2026-07-20 21:16:31 +0300

    Add fix.

--- src/settings/conf.py
+++ src/settings/conf.py
@@ -11,7 +11,7 @@ BOT_TOKEN = getenv('BOT_TOKEN', '')
 REDIS_URL = getenv('REDIS_URL', 'redis://localhost:6379/0')
 REDIS_FSM_KEY_PREFIX = getenv('REDIS_FSM_KEY_PREFIX', 'edelya_bot:fsm')
 MINI_APP_URL = getenv('MINI_APP_URL', '')
-SUPPORT_EMAIL = getenv('SUPPORT_EMAIL', 'edelya@corpdi.ru')
+SUPPORT_EMAIL = getenv('SUPPORT_EMAIL', 'edelya@corpdi.com')
 PROXY_URL = getenv('PROXY_URL', None)
 
 LOG_DIR = getenv('LOG_DIR', './logs')
